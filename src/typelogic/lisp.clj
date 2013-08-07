9383c293c6cf6f13eb2888510d51c8eae67a6d31
Sanshiro Yoshida <halcat0x15a@gmail.com>
2013-08-07 23:22:16 +0900

    refactoring and bug fix

--- src/typelogic/lisp.clj
+++ src/typelogic/lisp.clj
@@ -6,28 +6,47 @@
             [clojure.core.logic :refer :all]
             [clojure.core.logic.dcg :refer :all]))
 
-(def ^:dynamic *depth* 5)
+(def ^:dynamic *depth* 10)
+
+(defmacro error [s]
+  `(fn [a#] (prn ~s) nil))
 
 (declare ann)
 
-(defn -if [ctx consequence alternative type]
+(defna bind [ctx ctx' names types]
+  ([_ ctx [] []])
+  ([_ [[name type] . ctx''] ['& . _] [type]] (== type clojure.lang.ArraySeq))
+  ([_ [[name type] . ctx''] [name . names'] [type . types']]
+     (conda [(conda [(is type name #(some-> % meta :tag resolve))
+                     (pred type class?)])
+             (bind ctx ctx'' names' types')]
+            [(bind ctx ctx'' names' types')])))
+
+(defn ann-if [ctx consequence alternative type]
   (conde [(conda [(ann ctx consequence type)
                   (ann ctx alternative type)])]
          [(conde [(ann ctx consequence type)]
                  [(ann ctx alternative type)])]))
 
-(defna -let [ctx bindings body type]
+(defna ann-let [ctx bindings body type]
   ([_ [] _ _] (ann ctx body type))
   ([_ [var val . bindings'] _ _]
      (fresh [ctx' type']
        (ann ctx val type')
-       (conso [var type'] ctx ctx')
-       (-let ctx' bindings' body type))))
+       (bind ctx ctx' [var] [type'])
+       (ann-let ctx' bindings' body type))))
 
-(defna -do [ctx exprs type]
+(defna ann-do [ctx exprs type]
   ([_ [] nil])
   ([_ [expr] _] (ann ctx expr type))
-  ([_ [_ . exprs'] _] (-do ctx exprs' type)))
+  ([_ [_ . exprs'] _] (ann-do ctx exprs' type)))
+
+(defne ann-fn [ctx functions type]
+  ([_ [[parameters body] . _] [::-> types return]]
+     (fresh [ctx']
+       (bind ctx ctx' parameters types)
+       (ann ctx' body return)))
+  ([_ [_ . functions'] _] (ann-fn ctx functions' type)))
 
 (defna form [ctx exprs types]
   ([_ [] []])
@@ -37,12 +56,13 @@
 
 (defna subtyping [subtypes supertypes]
   ([[] []])
+  ([[type . subtypes'] [type . supertypes']] (subtyping subtypes' supertypes'))
+  ([_ [type]] (== type clojure.lang.ArraySeq))
   ([[subtype . subtypes'] [supertype . supertypes']]
      (project [subtype supertype] (== true (isa? subtype supertype)))
      (subtyping subtypes' supertypes')))
 
 (defna app [operator operands type]
-  ([[::-> operands type] operands _])
   ([[::-> supertypes type] _ _]
      (subtyping operands supertypes)))
 
@@ -50,67 +70,76 @@
   ([[operator . _] _ _] (app operator operands type))
   ([[_ . operators'] _ _] (attempt operators' operands type)))
 
-(defn tag [name type]
-  (conda [(project [name] (== type (some-> name meta :tag resolve)))
-          (pred type class?)]))
-
-(defna bind [ctx ctx' names types]
-  ([_ ctx [] []])
-  ([_ [[name type] . ctx''] [name . names'] [type . types']]
-     (conda [(tag name type)
-             (bind ctx ctx'' names' types')]
-            [(bind ctx ctx'' names' types')])))
-
-(defne abs [ctx functions type]
-  ([_ [[parameters body] . _] [::-> types return]]
-     (fresh [ctx']
-       (bind ctx ctx' parameters types)
-       (ann ctx' body return)))
-  ([_ [_ . functions'] _] (abs ctx functions' type)))
+(defn reflection [class method]
+  (->> class
+       .getMethods
+       (filter #(= (.getName %) (name method)))
+       (map #(list ::-> (vec (.getParameterTypes %)) (.getReturnType %)))))
 
 (defna variable [ctx expr type]
   ([[[expr type] . _] expr _])
-  ([[_ . ctx'] _ _] (ann ctx' expr type)))
+  ([[_ . ctx'] _ _] (variable ctx' expr type)))
 
-(defn reflection [instance method]
-  (some->>
-   instance
-   resolve
-   reflect
-   :members
-   (filter #(= (:name %) method))
-   (map #(list ::-> (map resolve (:parameter-types %)) (resolve (:return-type %))))))
+(defn immediate [expr type]
+    (conda [(pred expr symbol?)
+            (fresh [expr']
+              (is expr' expr #(some-> % source-fn read-string macroexpand-all))
+              (conda [(pred expr' (complement nil?))
+                      (ann [] expr' type)]
+                     [(fresh [var]
+                        (is var expr resolve)
+                        (pred var var?)
+                        (is type var (comp class var-get)))]
+                     [(project [expr]
+                        (error (str "Unable to resolve symbol: " expr)))]))]
+           [(conde [(pred expr integer?) (== type 'long)]
+                   [(pred expr float?) (== type 'double)]
+                   [(is type expr class)])]))
 
-(defne ann [ctx expr type]
-  ([_ ['def name expr'] [::def name type']] (ann ctx expr' type'))
-  ([_ ['fn* . functions] _] (abs ctx functions type))
-  ([_ ['if _ consequence] _] (-if ctx consequence nil type))
-  ([_ ['if _ consequence alternative] _] (-if ctx consequence alternative type))
-  ([_ ['let* bindings body] _] (-let ctx bindings body type))
-  ([_ ['do . exprs] _] (-do ctx exprs type))
+(defn call [ctx dot instance method arguments type]
+  (conda [(== '. dot)
+          (pred method symbol?)
+          (fresh [class arguments']
+            (form ctx arguments arguments')
+            (conde [(pred instance symbol?)
+                    (is class instance resolve)]
+                   [(ann ctx instance class)])
+            (conda [(pred class class?)
+                    (project [class method]
+                      (attempt (reflection class method) arguments' type))]
+                   [(project [instance]
+                      (error (str "Unable to typed symbol: " instance)))]))]))
+
+(defna ann [ctx expr type]
+  ([_ ['def name expr'] _]
+     (fresh [ctx']
+       (conso [name type] ctx ctx')
+       (ann ctx' expr' type)))
+  ([_ ['fn* name . functions] _]
+     (pred name symbol?)
+     (ann-fn ctx functions type))
+  ([_ ['fn* . functions] _] (ann-fn ctx functions type))
+  ([_ ['if _ consequence] _]
+     (ann-if ctx consequence nil type))
+  ([_ ['if _ consequence alternative] _]
+     (ann-if ctx consequence alternative type))
+  ([_ ['let* bindings body] _] (ann-let ctx bindings body type))
+  ([_ ['do . exprs] _] (ann-do ctx exprs type))
   ([_ ['quote] nil])
-  ([_ ['quote expr' . _] _] (project [expr'] (== type (class expr'))))
+  ([_ ['quote expr' . _] _] (immediate expr' type))
+  ([_ ['new class . _] _] (is type class resolve))
+  ([_ ['recur . _] _] u#)
   ([_ [dot instance [method . arguments]] _]
-     (fresh [arguments']
-       (== '. dot)
-       (form ctx arguments arguments')
-       (project [instance method]
-         (attempt (reflection instance method) arguments' type))))
-  ([_ ['new type' . _] _] (project [type'] (== type (resolve type'))))
+     (call ctx dot instance method arguments type))
+  ([_ [dot instance method . arguments] _]
+     (call ctx dot instance method arguments type))
   ([_ [operator . operands] _]
      (fresh [operator' operands']
        (ann ctx operator operator')
        (form ctx operands operands')
        (app operator' operands' type)))
   ([_ _ _] (variable ctx expr type))
-  ([_ _ _]
-     (project [expr]
-       (== false (symbol? expr))
-       (== false (seq? expr))
-       (== type (class expr)))))
+  ([_ _ _] (immediate expr type)))
 
 (defn check [expr]
   (set (run *depth* [type] (ann [] (macroexpand-all expr) type))))
-
-(defn check-fn [sym]
-  (-> sym source-fn read-string check))
--- test/typelogic/test.clj
+++ test/typelogic/test.clj
@@ -6,17 +6,17 @@
   (testing "if"
     (are [expr type] (= (check expr) type)
          '(if true "foo" "bar") #{String}
-         '(if true "" 0) #{String Long}
+         '(if true "" 0) #{String Long 'long}
          '(if true "") #{String nil}))
   (testing "do"
     (are [expr type] (= (check expr) type)
          '(do "") #{String}
          '(do) #{nil}
-         '(do "" 0) #{Long}))
+         '(do "" 0) #{Long 'long}))
   (testing "let"
     (are [expr type] (= (check expr) type)
          '(let [a ""] a) #{String}
-         '(let [] 0) #{Long}
+         '(let [] 0) #{Long 'long}
          '(let [a "" b true] b) #{Boolean}))
   (testing "fn"
     (are [expr type] (= (check expr) type)
@@ -27,10 +27,14 @@
          #{'[:typelogic.lisp/-> [_0] [:typelogic.lisp/-> [_1] _1]]}
          '(fn [^String a] a) #{[:typelogic.lisp/-> [String] String]}
          '(fn [^java.lang.String a] a) #{[:typelogic.lisp/-> [String] String]}))
+  (testing "quote"
+    (are [expr type] (= (check expr) type)
+         '(quote 0) #{Long 'long}
+         '(quote (fn [a] a)) #{clojure.lang.LazySeq}))
   (testing "app"
     (are [expr type] (= (check expr) type)
          '((fn [] "")) #{String}
-         '((fn [a] a) 0) #{Long}
+         '((fn [a] a) 0) #{Long 'long}
          '((fn [a b] b) 0 true) #{Boolean}
          '((fn [^Object a] a) "") #{Object}))
   (testing "dot"
@@ -42,4 +46,4 @@
          '(new Object) #{Object}
          '(new String "") #{String}))
   (testing "def"
-    (is (= (check '(def a "")) #{[:typelogic.lisp/def 'a String]}))))
+    (is (= (check '(def a "")) #{String}))))
