691f19a3e09aa497b2e081d8d693ab189d2c1eb6
Sanshiro Yoshida <halcat0x15a@gmail.com>
2014-08-30 00:52:29 +0900

    update rewriting rule

--- src/typelogic/core.clj
+++ src/typelogic/core.clj
@@ -92,7 +92,7 @@
            (core/isa? (get box parent parent) Number))
       (core/isa? child parent)))
 
-(defn subtype [sub super]
+#_(defn subtype [sub super]
   (conda
    [(== sub super)]
    [(project [sub super] (== (isa? sub super) true))]
@@ -101,6 +101,23 @@
     (ext-run-csg sub super)
     (project [sub] (log "after:" sub))]))
 
+(defn subtype [sub super]
+  (conda
+   [(== sub super)]
+   [(project [sub super] (== (isa? sub super) true))]
+   [(project [sub super] (== (isa? super sub) true))
+    (project [sub super] (log "before:" super))
+    (ext-run-csg super sub)
+    (project [sub] (log "after:" super))]))
+
+(defn ann- [env ctx type expr]
+  (fresh [subtype]
+    (ann env ctx subtype expr)
+    (conda [(== type subtype)]
+           [(project [type subtype] (== (isa? subtype type) true))]
+           [(project [type subtype] (== (isa? type subtype) true))
+            (ext-run-csg type subtype)])))
+
 (defn local [ctx type sym]
   (matcha [ctx]
     ([[[::var sym type] . _]])
@@ -108,41 +125,41 @@
       (nonlvaro ctx')
       (local ctx' type sym))))
 
-(defn global [ns env type symbol]
-  (binding [*ns* ns]
+(defn global [env ctx type symbol]
+  (binding [*ns* (::ns ctx)]
     (if-let [var (resolve symbol)]
       (if (class? var)
         (== type Class)
         (if-let [expr (source-fn symbol)]
-          (ann (:ns (meta var)) env [] type (read-string expr))
+          (ann env {::ns (:ns (meta var))} type (read-string expr))
           (throw (RuntimeException. "Source not found"))))
       (throw (RuntimeException. (str "Unable to resolve symbol: " symbol " in this context"))))))
 
-(defmulti special
-  (fn [ns env ctx type & exprs]
+(defmulti ann-app
+  (fn [env ctx type & exprs]
     (first exprs)))
 
-(defmethod special 'if
-  ([ns env ctx type tag]
+(defmethod ann-app 'if
+  ([env ctx type tag]
      (throw (RuntimeException. "Too few arguments to if")))
-  ([ns env ctx type tag test]
-     (special ns env ctx type tag))
-  ([ns env ctx type tag test then]
-     (special ns env ctx type tag test then nil))
-  ([ns env ctx type tag test then else]
+  ([env ctx type tag test]
+     (ann-app env ctx type tag))
+  ([env ctx type tag test then]
+     (ann-app env ctx type tag test then nil))
+  ([env ctx type tag test then else]
      (all
-       (fresh [type] (ann ns env ctx type test))
-       (ann ns env ctx type then)
-       (ann ns env ctx type else)))
-  ([ns env ctx type tag test then else & args]
+       (fresh [type] (ann env ctx type test))
+       (ann- env ctx type then)
+       (ann- env ctx type else)))
+  ([env ctx type tag test then else & args]
      (throw (RuntimeException. "Too many arguments to if"))))
 
-(defmethod special 'do [ns env ctx type tag & [expr & exprs' :as exprs]]
+(defmethod ann-app 'do [env ctx type tag & [expr & exprs' :as exprs]]
   (cond (empty? exprs) (== type nil)
-        (empty? exprs') (ann ns env ctx type expr)
+        (empty? exprs') (ann env ctx type expr)
         :else (all
-                (fresh [type] (ann ns env ctx type expr))
-                (apply special ns env ctx type tag exprs'))))
+                (fresh [type] (ann env ctx type expr))
+                (apply ann-app env ctx type tag exprs'))))
 
 (defn ann-tag [type sym]
   (all
@@ -150,105 +167,109 @@
     (is type sym reflect/tag)
     (pred type (complement nil?))))
 
-(defmethod special 'let* [ns env ctx type tag [var expr & bindings' :as bindings] & exprs]
+(defmethod ann-app 'let* [env ctx type tag [var expr & bindings' :as bindings] & exprs]
   (if (empty? bindings)
-    (apply special ns env ctx type 'do exprs)
+    (apply ann-app env ctx type 'do exprs)
     (fresh [val]
       (conda [(ann-tag val var)]
-             [(ann ns env ctx val expr)])
-      (apply special ns env (lcons [::var var val] ctx) type tag bindings' exprs))))
+             [(ann env ctx val expr)])
+      (apply ann-app env (assoc ctx var val) type tag bindings' exprs))))
 
-(defmethod special 'loop* [ns env ctx type tag bindings & exprs]
+(defmethod ann-app 'loop* [env ctx type tag bindings & exprs]
   (let [bindings (partition 2 bindings)]
-    (ann ns env ctx type `((fn* (~(vec (map first bindings)) ~@exprs)) ~@(map second bindings)))))
+    (ann env ctx type `((fn* (~(vec (map first bindings)) ~@exprs)) ~@(map second bindings)))))
 
-(defna bind [syms params bindings]
-  ([[] [] []])
-  ([['& sym] [[::seq]] [[::var sym [::seq]]]])
-  ([[sym . syms'] [param . params'] [[::var sym param] . bindings']]
-     (conda [(ann-tag param sym)]
-            [succeed])
-     (bind syms' params' bindings')))
+(defn ann-fn [env ctx return params [sym & syms' :as syms] body]
+  (cond (empty? syms) (all
+                        (emptyo params)
+                        (apply ann-app env (assoc ctx 'recur [::fn params return]) return 'do body))
+        (= sym '&) (fresh [param]
+                     (== params [[::seq]])
+                     (apply ann-app env (assoc ctx (first syms') param) return 'do body))
+        :else (fresh [param params']
+                (conso param params' params)
+                (if-let [tag (reflect/tag sym)]
+                  (== param tag)
+                  succeed)
+                (ann-fn env (assoc ctx sym param) return params' syms' body))))
 
-(defn ann-fn [ns env ctx type syms body]
-  (fresh [params return bindings ctx']
-    (== [::fn params return] type)
-    (bind syms params bindings)
-    (appendo (lcons [::var 'recur type] bindings) ctx ctx')
-    (apply special ns env ctx' return 'do body)))
+(defn ann-fns [env ctx return params [syms & body] & exprs]
+  (conde [(ann-fn env ctx return params syms body)]
+         [(if (empty? exprs)
+            fail
+            (apply ann-fns env ctx return params exprs))]))
 
-(defmethod special 'fn*
-  ([ns env ctx type tag] fail)
-  ([ns env ctx type tag expr & exprs]
-     (cond (seq? expr) (fresh [syms body]
-                         (logic/any (lcons syms body) (cons expr exprs))
-                         (project [body] (ann-fn ns env ctx type syms body)))
-           (vector? expr) (special ns env ctx type tag expr exprs)
-           (symbol? expr) (fresh [type']
-                            (apply special ns env (lcons [::var expr type'] ctx) type tag exprs)
-                            (fresh [type] (apply special ns env (lcons [::var expr type] ctx) type' tag exprs))))))
+(defmethod ann-app 'fn* [env ctx type tag expr & exprs]
+  (if (symbol? expr)
+    (fresh [type']
+      (apply ann-app env (assoc ctx expr type') type tag exprs)
+      (fresh [type]
+        (apply ann-app env (assoc ctx expr type) type' tag exprs)))
+    (fresh [params return]
+      (== type [::fn params return])
+      (cond (seq? expr) (apply ann-fns env ctx return params expr exprs)
+            (vector? expr) (ann-fn env ctx return params expr exprs)))))
 
-(defmethod special 'def
-  ([ns env ctx type tag name]
+(defmethod ann-app 'def
+  ([env ctx type tag name]
      (matcha [type] ([[::var name _]])))
-  ([ns env ctx type tag name expr]
+  ([env ctx type tag name expr]
      (let [macro? (:macro (meta name))]
        (if macro?
          fail
          (fresh [type' self]
            (== type [::var name type'])
-           (ann ns env (lcons [::var name self] ctx) type' expr)
-           (fresh [type] (ann ns env (lcons [::var name type] ctx) self expr))
+           (ann env (assoc ctx name self) type' expr)
+           (fresh [type] (ann env (assoc ctx name type) self expr))
            (append env type))))))
 
-(defmethod special 'quote [ns env ctx type tag & exprs]
+(defmethod ann-app 'quote [env ctx type tag & exprs]
   (== type (class (first exprs))))
 
-(defn app [ns env ctx params [arg & args' :as args]]
+(defn app [env ctx params [arg & args' :as args]]
   (if (empty? args)
     (matcha [params]
       ([[]])
       ([[[:seq]]]))
-    (matcha [params]
-      ([[[:seq]]]
-        (fresh [type] (ann ns env ctx type arg))
-        (app ns env ctx params args'))
-      ([[param . params']]
-        (fresh [type]
-          (ann ns env ctx type arg)
-          (subtype type param))
-        (app ns env ctx params' args')))))
+    (conda [(pred params #(= % [:seq]))
+            (fresh [type] (ann env ctx type arg))
+            (app env ctx params args')]
+           [(fresh [param params']
+              (conso param params' params)
+              #_(ext-run-csg params (lcons param params'))
+              (ann- env ctx param arg)
+              (app env ctx params' args'))])))
 
-(defmethod special 'new [ns env ctx type tag name & args]
+(defmethod ann-app 'new [env ctx type tag name & args]
   (let [class (resolve name)]
     (if (class? class)
       (fresh [params]
         (logic/any params (reflect/constructors class))
-        (app ns env ctx params args)
+        (app env ctx params args)
         (== type class))
       (throw (IllegalArgumentException. (str "Unable to resolve classname: " name))))))
 
-(defmethod special '. [ns env ctx type tag obj msg & args]
+(defmethod ann-app '. [env ctx type tag obj msg & args]
   (if (seq? msg)
-    (apply special ns env ctx type tag obj msg)
+    (apply ann-app env ctx type tag obj msg)
     (fresh [class]
       (or (if (symbol? obj)
             (let [var (resolve obj)]
               (if (class? var)
                 (== class var))))
           (fresh [type]
-            (ann ns env ctx type obj)
+            (ann env ctx type obj)
             (is class type convert)))
       (project [class]
-        (conda [(all
-                  (is type class #(reflect/field % msg))
-                  (pred type class?))]
-               [(fresh [params]
+        (conda [(fresh [params]
                   (logic/any [params type] (reflect/methods class msg))
-                  (app ns env ctx params args))])))))
+                  (app env ctx params args))]
+               #_[(all
+                  (is type class #(reflect/field % msg))
+                  (pred type class?))])))))
 
-(defmethod special 'throw [ns env ctx type tag e]
-  (ann ns env ctx Throwable e))
+(defmethod ann-app 'throw [env ctx type tag e]
+  (ann env ctx Throwable e))
 
 (defn ifn->fn [ifn fn]
   (conda [(matcha [ifn]
@@ -257,31 +278,34 @@
          [(pred ifn #(isa? % clojure.lang.IFn))
           (matcha [fn] ([[::fn _ _]]))]))
 
-(defmethod special :default [ns env ctx type f & args]
-  (fresh [ifn params]
-    (ann ns env ctx ifn f)
-    (ifn->fn ifn [::fn params type])
-    (app ns env ctx params args)))
+(defmethod ann-app :default [env ctx type f & args]
+  (fresh [ifn fn params]
+    (ann env ctx fn f)
+    #_(trace-s)
+    (ifn->fn ifn fn)
+    (ext-run-csg fn [::fn params type])
+    (app env ctx params args)))
 
-(defmethod special nil [ns env ctx type]
+(defmethod ann-app nil [env ctx type]
   (== type clojure.lang.PersistentList$EmptyList))
 
-(defn ann [ns env ctx type expr]
+(defn ann [env ctx type expr]
   (let [expr (macroexpand expr)]
-    (cond (symbol? expr) (conda [(membero [::var expr type] ctx)]
-                                [(global ns env type expr)])
-          (seq? expr) (apply special ns env ctx type expr)
+    (cond (symbol? expr) (conda [(pred ctx #(contains? % expr))
+                                 (ext-run-csg type (ctx expr))]
+                                [(global env ctx type expr)])
+          (seq? expr) (apply ann-app env ctx type expr)
           :else (== type (class expr)))))
 
 (defn check
   ([expr]
-     (check [] expr))
+     (check {} expr))
   ([ctx expr]
      (distinct
       (run* [q]
-        (fresh [env types]
-          (== q [env types])
-          (ann *ns* env ctx types expr)
+        (fresh [env type]
+          (== q [env type])
+          (ann env (assoc ctx ::ns *ns*) type expr)
           (freeze env))))))
 
 (defn read-env [expr]
@@ -304,3 +328,5 @@
     (if-not (empty? exprs)
       (let [[env type] (check ctx expr)]
         (recur (concat (read-env env) ctx) exprs' (cons type types))))))
+
+(check '(defn f [a] (if true 0 (f ""))))
